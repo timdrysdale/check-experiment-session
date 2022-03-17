1a6d3b6b5f50b965be1352b9fd0300272c24db33
Tim Drysdale <timothy.d.drysdale@gmail.com>
2022-03-17 22:48:53 +0000

    Switch to production mode

--- scripts/example.sh
+++ scripts/example.sh
@@ -3,5 +3,5 @@ export PLAY_EXPT=spin35
 export PLAY_RESULT_DIR=$HOME/tmp
 export PLAY_DURATION=90
 export PLAY_FILE="../files/step.play"
-export SESSION_CLIENT_FILE_DEVELOPMENT=true
+#export SESSION_CLIENT_FILE_DEVELOPMENT=true
 ./play.sh
